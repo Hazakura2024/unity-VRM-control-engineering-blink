91faa90ed3445f363f21002a9dd47d90e393d3a0
Hazakura2024 <ytr240.jp@gmail.com>
2026-01-06 21:05:51 +0900

    feat: 制御工学瞬き機能を作成

--- Assets/Scripts/AutoBlink.cs
+++ Assets/Scripts/AutoBlink.cs
@@ -1,11 +1,28 @@
+using System;
 using System.Collections;
 using Unity.VisualScripting;
+using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 // VRM1.0を使用
 using UniVRM10;
 
 public class AutoBlink : MonoBehaviour
 {
+    // 制御パラメータ
+    // 固有角周波数ω
+    public float omega_n = 30.0f;
+    // 減衰係数
+    public float zeta = 1.0f;
+
+    // 状態変数
+    // まぶたの位置
+    private float x = 0.0f;
+    // まぶたの速度
+    private float v = 0.0f;
+    // 目標位置r(t)
+    private float target = 0.0f;
+
+
     // VRMを制御するコンポーネント
     private Vrm10Instance vrmInstance;
 
@@ -33,15 +50,34 @@ public class AutoBlink : MonoBehaviour
         // 時間が来たらまばたき実行
         if (timer >= nextBlinkTime)
         {
-            StartCoroutine(BlinkAction());
+            // StartCoroutine(BlinkAction());
+            StartCoroutine(BlinkInpulse());
             ResetTimer();
         }
+
+        // 制御工学計算
+        // 運動方程式: x'' + 2*zeta*omega*x' + omega^2*x = omega^2*r
+        // 加速度 a = x'' を求める
+            float a = (omega_n * omega_n * target) 
+                      - (2 * zeta * omega_n * v) 
+                      - (omega_n * omega_n * x);
+
+        // オイラー法で積分
+        float dt = Time.deltaTime;
+        v += a * dt;
+        x += v * dt;
+
+        // 値を0~1にclamp
+        float appliedWeight = Mathf.Clamp01(x);
+
+        //vrmに適用
+        vrmInstance.Runtime.Expression.SetWeight(ExpressionKey.Blink, appliedWeight);
     }
 
     // 指数分布に従う乱数生成（平均間隔 meanTime 秒）
     float GenerateExponentialRandom(float meanTime)
     {
-        return -meanTime * Mathf.Log(Random.value);
+        return -meanTime * Mathf.Log(UnityEngine.Random.value);
     }
 
     // まばたきの動作
@@ -56,12 +92,23 @@ public class AutoBlink : MonoBehaviour
         // 目を開ける(Blinkの値を0.1f)
         vrmInstance.Runtime.Expression.SetWeight(ExpressionKey.Blink, 0.0f);
     }
+    IEnumerator BlinkInpulse()
+    {
+        // ステップ入力(閉じる)
+        target = 1.0f;
+        // 0.1秒待つ(閉じてる時間)
+        yield return new WaitForSeconds(0.1f);
+        // ステップ入力(開く)
+        target = 0.0f;
+    }
+
+
 
     void ResetTimer()
     {
         timer = 0f;
 
-        // 2秒~5秒の間のランダムに次の瞬きを決める
-        nextBlinkTime = Random.Range(2.0f, 5.0f);
+        // 平均3.5秒間のランダムに次の瞬きを決める
+        nextBlinkTime = GenerateExponentialRandom(3.5f);
     }
 }
